6284b05f46c3b2cdac50d8b2a06a82adf357a4d7
eduardmelkonyan <melkon1999@gmail.com>
2021-10-08 23:38:01 +0400

    cube

--- cube.js
+++ cube.js
@@ -1,13 +1,31 @@
 const str = 'henc hima hardzakvel';
 let newArray = str.split('');
 const cub = Math.ceil(str.length/8);
+const array = [];
 
-function foo (arr) {
-    const array = [];
-    while(arr.length) {
+function foo(arr) {
+   while(arr.length) {
         array.push(arr.splice(0,8));
     }
     return array;
 }
 
-console.log(foo(newArray));
+function up(array, i) {
+    i = 0;
+    let tmp = array[i][0]
+    let tmp1 = array[i][1];
+    let tmp2 = array[i][2];
+    let tmp3 = array[i][6];
+    array[i][0] = array[i][4];
+    array[i][1] = tmp;
+    array[i][2] = array[i][3];
+    array[i][3] = array[i][7];
+    array[i][4] = array[i][5];
+    array[i][5] = tmp1;
+    array[i][6] = tmp2;
+    array[i][7] = tmp3;
+    return array;
+}
+
+foo(newArray);
+console.log(up(array));
