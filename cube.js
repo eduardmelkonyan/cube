4c3b6967c9a5481962c107fdda88e232efb50779
eduardmelkonyan <melkon1999@gmail.com>
2021-10-11 18:20:14 +0400

    finished

--- cube.js
+++ cube.js
@@ -1,4 +1,4 @@
-const str = 'henc hima hardzakvel';
+const str = 'Հենց հիմա հարձակվել!';
 let newArray = str.split('');
 const cub = Math.ceil(str.length/8);
 const array = [];
@@ -11,7 +11,6 @@ function foo(arr) {
 }
 
 function up(array, i) {
-    i = 0;
     let tmp = array[i][0]
     let tmp1 = array[i][1];
     let tmp2 = array[i][2];
@@ -27,5 +26,56 @@ function up(array, i) {
     return array;
 }
 
+function right(array, i) {
+    let tmp = array[i][0];
+    let tmp1 = array[i][4];
+    array[i][0] = array[i][1];
+    array[i][1] = array[i][2];
+    array[i][2] = array[i][3];
+    array[i][3] = tmp;
+    array[i][4] = array[i][5];
+    array[i][5] = array[i][6];
+    array[i][6] = array[i][7];
+    array[i][7] = tmp1;
+    return array;
+}
+
+function left(array, i) {
+    let tmp = array[i][0];
+    let tmp1 = array[i][2];
+    let tmp2 = array[i][4];
+    let tmp3 = array[i][5];
+    let tmp4 = array[i][6];
+    let tmp5 = array[i][1];
+    array[i][0] = array[i][3];
+    array[i][1] = tmp;
+    array[i][2] = tmp5;
+    array[i][3] = tmp1;
+    array[i][4] = array[i][7];
+    array[i][5] = tmp2;
+    array[i][6] = tmp3;
+    array[i][7] = tmp4;
+    return array;
+}
+
+function down(array, i) {
+    let tmp = array[i][0];
+    let tmp1 = array[i][2];
+    let tmp2 = array[i][3];
+    let tmp3 = array[i][4];
+    array[i][0] = array[i][1];
+    array[i][1] = array[i][5];
+    array[i][2] = array[i][6];
+    array[i][3] = tmp1;
+    array[i][4] = tmp;
+    array[i][5] = tmp3;
+    array[i][6] = array[i][7];
+    array[i][7] = tmp2;
+    return array;
+}
+
 foo(newArray);
-console.log(up(array));
+up(array,0);
+right(array,1);
+left(array,2);
+console.log(array);
