ca99d45c51cf486bbf79db360f5daebabf38540b
Eduard <melkon1999@gmail.com>
2022-02-07 17:30:33 +0400

    updated some codes

--- cube.js
+++ cube.js
@@ -1,86 +1,86 @@
-// Imagine a war situation. Comrade-in-arms should explain other actions to their friends. 
+// Imagine a war situation. Comrade-in-arms should explain other actions to their friends.
 // They must send encrypted text so that the enemy does not understand․
-const str = 'Attack right now!';
-let newArray = str.split('');
-const cub = Math.ceil(str.length/8);
-const array = [];
+const message = "Attack right now!";
+let newCodedMessage = message.split("");
+const cube = Math.ceil(message.length / 8);
+const codedMessage = [];
 
 // This function creates the appropriate number of cubes and placing each symbol in the corners of the cube
 function devideIntoCubes(arr) {
-   while(arr.length) {
-        array.push(arr.splice(0,8));
-    }
-    return array;
+  while (arr.length) {
+    codedMessage.push(arr.splice(0, 8));
+  }
+  return codedMessage;
 }
 
 // Then we call these functions up, down, to the right, to the left to turn the corresponding cube.
 
-function up(array, i) {
-    let tmp = array[i][0]
-    let tmp1 = array[i][1];
-    let tmp2 = array[i][2];
-    let tmp3 = array[i][6];
-    array[i][0] = array[i][4];
-    array[i][1] = tmp;
-    array[i][2] = array[i][3];
-    array[i][3] = array[i][7];
-    array[i][4] = array[i][5];
-    array[i][5] = tmp1;
-    array[i][6] = tmp2;
-    array[i][7] = tmp3;
-    return array;
+function up(codedMessage, i) {
+  let tmp = codedMessage[i][0];
+  let tmp1 = codedMessage[i][1];
+  let tmp2 = codedMessage[i][2];
+  let tmp3 = codedMessage[i][6];
+  codedMessage[i][0] = codedMessage[i][4];
+  codedMessage[i][1] = tmp;
+  codedMessage[i][2] = codedMessage[i][3];
+  codedMessage[i][3] = codedMessage[i][7];
+  codedMessage[i][4] = codedMessage[i][5];
+  codedMessage[i][5] = tmp1;
+  codedMessage[i][6] = tmp2;
+  codedMessage[i][7] = tmp3;
+  return codedMessage;
 }
 
-function right(array, i) {
-    let tmp = array[i][0];
-    let tmp1 = array[i][4];
-    array[i][0] = array[i][1];
-    array[i][1] = array[i][2];
-    array[i][2] = array[i][3];
-    array[i][3] = tmp;
-    array[i][4] = array[i][5];
-    array[i][5] = array[i][6];
-    array[i][6] = array[i][7];
-    array[i][7] = tmp1;
-    return array;
+function right(codedMessage, i) {
+  let tmp = codedMessage[i][0];
+  let tmp1 = codedMessage[i][4];
+  codedMessage[i][0] = codedMessage[i][1];
+  codedMessage[i][1] = codedMessage[i][2];
+  codedMessage[i][2] = codedMessage[i][3];
+  codedMessage[i][3] = tmp;
+  codedMessage[i][4] = codedMessage[i][5];
+  codedMessage[i][5] = codedMessage[i][6];
+  codedMessage[i][6] = codedMessage[i][7];
+  codedMessage[i][7] = tmp1;
+  return codedMessage;
 }
 
-function left(array, i) {
-    let tmp = array[i][0];
-    let tmp1 = array[i][2];
-    let tmp2 = array[i][4];
-    let tmp3 = array[i][5];
-    let tmp4 = array[i][6];
-    let tmp5 = array[i][1];
-    array[i][0] = array[i][3];
-    array[i][1] = tmp;
-    array[i][2] = tmp5;
-    array[i][3] = tmp1;
-    array[i][4] = array[i][7];
-    array[i][5] = tmp2;
-    array[i][6] = tmp3;
-    array[i][7] = tmp4;
-    return array;
+function left(codedMessage, i) {
+  let tmp = codedMessage[i][0];
+  let tmp1 = codedMessage[i][2];
+  let tmp2 = codedMessage[i][4];
+  let tmp3 = codedMessage[i][5];
+  let tmp4 = codedMessage[i][6];
+  let tmp5 = codedMessage[i][1];
+  codedMessage[i][0] = codedMessage[i][3];
+  codedMessage[i][1] = tmp;
+  codedMessage[i][2] = tmp5;
+  codedMessage[i][3] = tmp1;
+  codedMessage[i][4] = codedMessage[i][7];
+  codedMessage[i][5] = tmp2;
+  codedMessage[i][6] = tmp3;
+  codedMessage[i][7] = tmp4;
+  return codedMessage;
 }
 
-function down(array, i) {
-    let tmp = array[i][0];
-    let tmp1 = array[i][2];
-    let tmp2 = array[i][3];
-    let tmp3 = array[i][4];
-    array[i][0] = array[i][1];
-    array[i][1] = array[i][5];
-    array[i][2] = array[i][6];
-    array[i][3] = tmp1;
-    array[i][4] = tmp;
-    array[i][5] = tmp3;
-    array[i][6] = array[i][7];
-    array[i][7] = tmp2;
-    return array;
+function down(codedMessage, i) {
+  let tmp = codedMessage[i][0];
+  let tmp1 = codedMessage[i][2];
+  let tmp2 = codedMessage[i][3];
+  let tmp3 = codedMessage[i][4];
+  codedMessage[i][0] = codedMessage[i][1];
+  codedMessage[i][1] = codedMessage[i][5];
+  codedMessage[i][2] = codedMessage[i][6];
+  codedMessage[i][3] = tmp1;
+  codedMessage[i][4] = tmp;
+  codedMessage[i][5] = tmp3;
+  codedMessage[i][6] = codedMessage[i][7];
+  codedMessage[i][7] = tmp2;
+  return codedMessage;
 }
 
-devideIntoCubes(newArray);
-down(array,0);
-left(array,1);
-right(array,2);
-console.log(array);
+devideIntoCubes(newCodedMessage);
+down(codedMessage, 0);
+left(codedMessage, 1);
+right(codedMessage, 2);
+console.log(codedMessage);
